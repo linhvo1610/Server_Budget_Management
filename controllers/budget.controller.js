39c181d6a50776af5f77d644cbef59f6001dbbca
laptop <PatuanDev>
2023-06-14 02:20:33 +0700

    Phùng Anh Tuấn Code budget , sửa giao diện

--- controllers/budget.controller.js
+++ controllers/budget.controller.js
@@ -1,3 +1,4 @@
+const { render } = require('ejs');
 const myModel = require('../models/model');
 const fs = require('fs');
 
@@ -18,11 +19,74 @@ exports.getrecord = async (req, res, next) => {
     res.render('budget/expense', { list: list, list_cat: list_cat },)
 
 }
+// exports.chitiet = async (req,res,next) =>{
+//     let objbuget = await myModel.recordModel.findById(req.params.idcat);
+//     console.log(objbuget);
+//     let objcat = await myModel.categoryModel.find();
+//     let objuser = await myModel.usersModel.find();
+//     let objbal= await myModel.balanceModel.find();
+//     if(req.method == 'POST') {
+//         let obj = new myModel.recordModel();
+//         obj.id_balance=req.body.id_balance;
+//         obj.id_user=req.body.id_user;
+//         obj.id_cat=req.body.id_cat;
+//         obj.is_expense=req.body.is_expense;
+//         obj.title=req.body.title;
+//         obj.price=req.body.price;
+//         obj.date=req,body.date;
+//         try {
+//             await myModel.recordModel.findByIdAndUpdate({_id: req.params.idcat}, obj);
+//             console.log("da update")
+            
+//         } catch (err) {
+//             console.log(err);
+            
+//         }
+//         res.render('budget/detail', { objbuget: objbuget, objuser:objuser, objcat:objcat,objbal:objbal })
+//     }
+    
+    
+
+    // let objSp = await mymodel.spModel.findById(req.params.idsp);
+    // console.log(objSp);
+    // let listthe_loai = await mymodel.theloaimodel.find();
+
+    // if (req.method == 'POST') {
+    //     let objSP = new mymodel.spModel();
+    //     objSP.image = req.file.filename;
+    //     objSP.title = req.body.title;
+    //     objSP.name = req.body.name;
+    //     objSP.price = req.body.price;
+    //     objSP.content = req.body.content;
+    //     objSP.the_loai = req.body.theloai;
+    //     objSP._id = req.params.idsp;
+    //     try {
+    //         // update dữ liệu
+    //         // await myModel.spModel.updateOne( {_id:  req.params.idsp},   objSP );
+    //         await mymodel.spModel.findByIdAndUpdate({ _id: req.params.idsp }, objSP);
+
+    //         console.log("Đã ghi thành công");
+    //         msg = 'Đã ghi thành công';
+    //     } catch (err) {
+    //         console.log(err);
+    //         msg = 'Lỗi ' + err.message;
+    //     }
+
+    // }
+
+    // res.render('sanpham/update_pr', { title: 'updateProduct', msg: msg, objSp: objSp, listTheLoai: listthe_loai })
+
+
+
+
 exports.detail = async(req,res,next) =>{
+
+
+    
+    // let dieu_kien = null;\
     let dieu_kien = null;
     let list_cat = await myModel.categoryModel.find();
     let objCat = await myModel.categoryModel.findById(req.params.idcat);
-   
     console.log(objCat);
     if (typeof (req.query.id_cat) != 'undefined') {
         let id_cat = req.query.id_cat;
@@ -33,7 +97,7 @@ exports.detail = async(req,res,next) =>{
 
     var list = await myModel.recordModel.find(dieu_kien).populate('id_cat');
     console.log(list);
-    res.render('budget/detail', { list: list, list_cat: list_cat },)
+    res.render('budget/detail', { list: list, list_cat: list_cat, })
 }
 exports.addrecord = async (req, res, next) => {
     var url_img = '';
@@ -137,3 +201,41 @@ exports.addrecord = async (req, res, next) => {
 //     res.render( 'budget/add_receive_budget',{list_cat:list_cat,list_user:list_user},)
 
 // }
+// exports.sortuser = async(req,res,next)=>{
+//     //Hiển thị danh sach san pham
+    
+//     let dieu_kien = null;
+//     let list_cat = await myModel.categoryModel.find();
+//     let objCat = await myModel.categoryModel.findById(req.params.idcat);
+//     console.log(objCat);
+//     if (typeof (req.query.id_cat) != 'undefined') {
+//         let id_cat = req.query.id_cat;
+//         dieu_kien = { id_cat: id_cat };
+        
+
+//     }
+
+
+//     var list = await myModel.recordModel.find(dieu_kien).populate('id_cat');
+//     console.log(list);
+//     res.render('budget/expense', { list: list, list_cat: list_cat },)
+
+// }
+exports.sortuser = async(req,res,next)=>{
+    //Hiển thị danh sach san pham
+    
+    //kiểm tra tồn tại tham số
+    let dieu_kien =null;
+    if(typeof(req.query.title)!='undefined'){
+        let title =req.query.title;
+        dieu_kien={title:title};
+    }
+    
+
+    //var list=await myModel.spModel.find(dieu_kien).sort({name:1});
+    //cair tieens lay them the loai
+    var list=await myModel.recordModel.find().sort({title:-1});
+    console.log(list);
+
+    res.render('budget/expense',{list:list})
+}
--- routes/budget.js
+++ routes/budget.js
@@ -6,10 +6,11 @@ var budgetcontroller = require('../controllers/budget.controller');
 var multer=require('multer');
 var uploader=multer({dest:'./tmp'});
 router.get('/list',check_login.request_login,budgetcontroller.getrecord);
-router.get('/detail/:idcat',check_login.request_login,budgetcontroller.detail);
+router.get('/detail/:id_cat',check_login.request_login,budgetcontroller.detail);
 
 router.get('/add',check_login.request_login,budgetcontroller.addrecord);
 router.post('/add',check_login.request_login,budgetcontroller.addrecord);
+router.get('/tim',check_login.request_login,budgetcontroller.sortuser);
 // router.get('/addreceivebudget',check_login.request_login,budgetcontroller.addreceivebudget);
 // router.post('/addreceivebudget',check_login.request_login,budgetcontroller.addreceivebudget);
 module.exports = router;
