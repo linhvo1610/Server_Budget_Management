02119fb4f5bfe101783a86b733fa8405a41a8784
laptop <PatuanDev>
2023-06-22 01:41:08 +0700

    da push

--- models/model.js
+++ models/model.js
@@ -2,7 +2,7 @@ var db = require('./db');
 const userSchema = new db.mongoose.Schema(
     {
         email: { type: String, required: true },
-        username:{type:String,required:true},
+        username:{type:String,required:true,unique:true},
         password:{type:String,required:true},
         role:{type:String,required:true},
 
